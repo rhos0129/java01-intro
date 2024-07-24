be5cf2fb4129a4752c7f2fe179ff42270557aa10
rhos0129 <rhos0129@gmail.com>
2024-07-24 18:37:21 +0900

    S01 자바 프로그램 실행

---
+++ src/HelloJava.java
@@ -0,0 +1,13 @@
+public class HelloJava { // 클래스 (파일)
+
+    public static void main(String[] args) { // main 메서드 (자바 프로그램의 시작점)
+        System.out.println("hello java"); // 콘솔에 값 출력
+
+        // 프로그램 실행 과정
+        // 1. HelloJava 프로그램을 실행한다.
+        // 2. 자바는 시작점인 main() 메서드를 실행한다.
+        // 3. System.out.println("hello java") 을 만나고, 문자열 hello java 을 출력한다.
+        // 4. main() 메서드의 {} 블록이 끝나면 프로그램은 종료된다.
+    }
+
+}
