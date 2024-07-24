7f4da2bd612c53598fc69efcb44448e941a22375
rhos0129 <rhos0129@gmail.com>
2024-07-24 18:39:43 +0900

    S01 주석(comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,17 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1");
+
+        // 한줄 주석
+        //System.out.println("hello java2");
+
+        // 여러줄 주석
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+
+        // 자바는 주석이 있는 곳을 무시한다.
+    }
+}
