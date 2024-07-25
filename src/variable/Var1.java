b51c3921d448e10920e2ccb312d83d6c902f10aa
rhos0129 <rhos0129@gmail.com>
2024-07-25 14:40:11 +0900

    S02 변수 시작

---
+++ src/variable/Var1.java
@@ -0,0 +1,14 @@
+package variable; // 패키지 (폴더)
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(20); //변경 10 -> 20
+        System.out.println(20); //변경 10 -> 20
+        System.out.println(20); //변경 10 -> 20
+
+        // 10을 100번 출력해야한다면 100개의 코드를 변경해야한다.
+        // 만약 사용자가 입력한 숫자를 출력하고 싶다면?
+        // => 어딘가에 값을 보관해두고 필요할 때 값을 꺼내서 읽을 수 있는 저장소 변수가 필요
+    }
+}
---
+++ src/variable/Var2.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class Var2 {
+
+    public static void main(String[] args) {
+
+        // 변수 선언
+        // 숫자 정수(integer)를 보관할 수 있는 이름이 a 라는 데이터 저장소 생성
+        int a;
+
+        // 변수에 값 대입
+        // 자바에서 = 은 오른쪽에 있는 값을 왼쪽에 저장한다는 뜻 (수학에서의 =와는 다름)
+        // 변수 초기화 : 선언한 변수에 처음으로 값을 대입해서 저장하는 것
+        a = 20; //10 -> 20
+
+        // 변수 값 읽기
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+
+}
