4832a9683f15d50d0c866b5dd3d3a8ec2ba1ef00
rhos0129 <rhos0129@gmail.com>
2024-07-25 15:02:45 +0900

    S02 변수 선언과 초기화

---
+++ src/variable/Var4.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var4 {
+
+    public static void main(String[] args) {
+
+        // 변수 하나씩 선언
+        int a;
+        int b;
+
+        // 여러 변수를 한번에 선언
+        int c,d;
+
+        // 변수 선언
+        // 데이터를 보관할 수 있는 공간을 만들고, 그곳에 이름을 부여
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+
+        int a;
+//        System.out.println(a);  // 주석을 풀면 컴파일 에러 발생
+
+        // 변수의 초기화
+        // 초기화 하지 않으면 기존 메모리 공간에 어떤 값이 있었는지 모르기에 이상한 값이 출력될 수도 있다.
+        // 자바는 반드시 변수를 초기화 하도록 강제한다.
+
+    }
+
+}
