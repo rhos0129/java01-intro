515b3abdc1781688e6c2f7c0dcd819de58683181
rhos0129 <rhos0129@gmail.com>
2024-07-25 15:45:04 +0900

    S02 문제와 풀이

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,19 @@
+package variable.ex;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+
+        // 다음 코드에 반복해서 나오는 숫자 4 , 3 을 다른 숫자로 한번에 변경할 수 있도록
+        // 변수 num1 , num2 를 사용하여 변경해보세요.
+//        System.out.println(4 + 3);
+//        System.out.println(4 - 3);
+//        System.out.println(4 * 3);
+
+        int num1 = 10;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+
+    }
+}
