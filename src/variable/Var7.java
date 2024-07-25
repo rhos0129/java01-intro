676115eb7cf955e8e4f5d3fa8fb3012dc9c0bfd5
rhos0129 <rhos0129@gmail.com>
2024-07-25 15:20:28 +0900

    S02 변수 타입1

---
+++ src/variable/Var7.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true; //불리언(boolean) true, false 입력 가능
+        char d = 'A'; //문자 하나
+        String e = "Hello Java"; //문자열, 문자열을 다루기 위한 특별한 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        // 변수는 자신의 타입에 맞는 데이터 사용해야 한다. 불일치 시 컴파일 오류 발생한다.
+        // 리터럴 : 코드에서 개발자가 직접 적은 고정된 값. 변수의 값은 변할 수 있지만 리터럴 자체는 변하지 않는다.
+    }
+}
