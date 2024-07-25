c1c7e5bad843181d6473f5cbd304740a081ace04
rhos0129 <rhos0129@gmail.com>
2024-07-25 14:50:44 +0900

    S02 변수 값 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+
+        int a;//변수 선언
+        a = 10; //변수 초기화 a(10)
+        System.out.println(a);
+
+        a = 50; //변수 값 변경: a(10 -> 50)
+        System.out.println(a);
+
+        // 변수 a 에 저장된 값을 언제든지 바꿀 수 있다
+        // 변수의 값을 변경하면 변수에 들어있던 기존 값은 삭제
+
+    }
+
+}
