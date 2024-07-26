d15191f836880d98d75b1052111a30e50328c293
rhos0129 <rhos0129@gmail.com>
2024-07-26 17:52:56 +0900

    S05 for문1

---
+++ src/loop/For2.java
@@ -0,0 +1,24 @@
+package loop;
+
+public class For2 {
+
+    public static void main(String[] args) {
+
+        // i부터 하나씩 증가하는 수를 endNum(마지막 수)까지 더해라
+
+        int sum = 0;
+        int endNum = 3;
+
+        //sum=1+2+3
+        for (int i = 1; i <= endNum; i++) {
+            sum = sum + i;
+            System.out.println("i=" + i + " sum=" + sum);
+        }
+
+        // While2_3와 비교하면 for문이 더 깔끔하다.
+        // for문 -> 변수 초기화, 조건식, 증감식이 모두 한 줄에 있다.
+        // while문 -> 해당 코드가 모두 분산되어 있다.
+
+    }
+
+}
